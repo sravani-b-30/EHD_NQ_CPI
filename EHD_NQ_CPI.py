6cca501527f97758b9dcba679adc8a660436b630
B.Sravani <sravani@napqueens.com>
2024-10-16 18:36:57 +0530

    modified dd to pd.numeric of price column in serp data

--- EHD_NQ_CPI.py
+++ EHD_NQ_CPI.py
@@ -367,7 +367,7 @@ def load_and_preprocess_data(folder, static_file_name, price_data_prefix):
     merged_data_df = merged_data_df.rename(columns={"ASIN": "asin", "title": "product_title"})
     merged_data_df['asin'] = merged_data_df['asin'].astype(str).str.upper()
     merged_data_df['ASIN'] = merged_data_df['asin']
-    merged_data_df['price'] = dd.to_numeric(merged_data_df['price'], errors='coerce')
+    merged_data_df['price'] = pd.to_numeric(merged_data_df['price'], errors='coerce')
     merged_data_df = df_scrapped_cleaned.merge(merged_data_df[['asin', 'product_title', 'price', 'date']], left_on='ASIN', right_on='asin', how='left')
 
     # Load price data specific to the brand
